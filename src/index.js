0a20b5cce2d5d7e9e46b0b20d19aeb9a1309aab5
Vasiliy Smirnov <vasya.smirnoff24@yandex.ru>
2020-06-03 14:09:46 +0300

    Done"

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
-class Character {
-  constructor(name) {
-    this.name = name;
-    this.level = 1;
-    this.health = 100;
-}
+export class ErrorRepository {
+  constructor(map) {
+    this.map = new Map()
+    this.map.set(1, 'Syntax error').set(2, 'Reference error').set(3, 'Type error')
+  }
+  translate(code) {
+    if (this.map.has(code) === true) {
+    return this.map.get(code);
+    } else if (this.map.has(code) === false) {
+      return 'Unknown error'
+    }
+  }
 }
 
-export { Character }
+
